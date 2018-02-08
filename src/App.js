de2dcea850119700786491edd6cb69c59e3317fa
Fan Cheung <fcheung@exchange.slb.com>
2018-02-08 17:34:04 +0800

    convert to functional

--- src/App.js
+++ src/App.js
@@ -1,134 +1,145 @@
-import React, {Component} from 'react'
-import {Observable, Subject} from 'rxjs/Rx'
-import {map, fromEvent, of, from} from 'rxjs/Rx'
+import React, {
+    Component
+} from 'react'
+import {
+    Observable,
+    Subject
+} from 'rxjs/Rx'
+
 import logo from './logo.svg'
 import './App.css'
 import * as CanvasHelper from './canvas-helper'
 var convert = require('color-convert');
 
 class App extends Component {
-  constructor() {
-    super()
-    this.image = new Image()
-  }
-
-  config() {
-    const resolution = 32
-    let minDimension = 64
-    let scale = 1
-    // set sprite to pixel art dimension
-    this.sprite.height = HEIGHT * scale;
-    this.sprite.width = WIDTH * scale;
-    const ctxSprite = this
-      .sprite
-      .getContext('2d')
-    ctxSprite.mozImageSmoothingEnabled = false;
-    ctxSprite.webkitImageSmoothingEnabled = false;
-    ctxSprite.imageSmoothingEnabled = false;
-
-    return of({resolution, minDimension, scale, ctxSprite})
-  }
-
-  componentDidMount() {
-
-    config().map((e) => e)
-
-    fromEvent(document.querySelector('#file-upload'), 'change').do 
-      (event => {
-        let file = event.target.files[0];
-        if (file.type.indexOf("image") === 0) {
-          reader.readAsDataURL(file);
-        }
-      })
-
-    let resolutionChange = fromEvent(this.resolution, 'change').map(e => e.target.value)
-    let fileRead = fromEvent(new FileReader(), 'load')
-    let draw = () => fileRead.map()
-    let imageLoaded = fromEvent(new Image(), 'load').map(img => {})
-    let trimmedDemension = 0
-
-    let getOptimisedDimension = (img) => {
-      return (img.width < img.height)
-        ? img.width
-        : img.height
+    constructor() {
+        super()
+        this.image = new Image()
     }
 
-    ctxSprite.drawImage(image, 0, 0, trimmedDemension, trimmedDemension, 0, 0, HEIGHT / BLOCK_SIZE, WIDTH / BLOCK_SIZE)
-    // ctxSprite.drawImage(sprite, 0, 0, WIDTH / BLOCK_SIZE, HEIGHT / BLOCK_SIZE, 0,
-    // 0, WIDTH, HEIGHT)
-    ctxSprite.drawImage(sprite, 0, 0, RESOLUTION, RESOLUTION, 0, 0, WIDTH * 2, HEIGHT * 2)
-    // context.drawImage(image, 0, 0, preview.width, preview.height) round to 256
-    let colorData = ctxSprite
-      .getImageData(0, 0, WIDTH, HEIGHT)
-      .data
+    componentDidMount() {
+        const fromEvent=Observable.fromEvent
+        const from=Observable.from
+        const of=Observable.of
+        // config().map((e) => e)
+        const resolution = 32
+        let minDimension = 64
+        // set sprite to pixel art dimension
+        const ctxSprite = this
+            .sprite
+            .getContext('2d')
+       
+        let fileUploaded = Observable.fromEvent(this.fileUpload, 'change')
+            .map(e => e.target.files[0])
 
-    // TODO: remove temp and improve algorithm
-    let data = colorData.reduce((acc, curr, index, arr) => {
-      // let key = Math.floor(index / 4)
-      acc
-        .temp
-        .push(curr)
-      if (acc.temp.length === 4) {
-        const ansi = convert
-          .rgb
-          .ansi256(...acc.temp)
+        let resolutionChange = fromEvent(this.resolution, 'change').map(e => e.target.value)
 
-        const rounded = convert
-          .ansi256
-          .rgb(ansi)
-          .concat(255)
+        let readerFile = (file) => {
+            let reader = new FileReader()
+            reader.readAsDataURL(file)
+            return fromEvent(reader, 'load')
+                .map(e => e.target.result)
 
-        acc.data = acc
-          .data
-          .concat(rounded)
-        acc.temp = []
-      }
+        }
+        let scaleChange = fromEvent(this.scale, 'change').map(e => e.target.value)
+        let imageLoaded = (imageData) => {
+            const image = new Image()
+            image.src = imageData
+            return fromEvent(image, 'load').map(e=>e.target)
+        }
 
-      return acc
-    }, {
-      temp: [],
-      data: []
+        let getOptimisedDimension = (image) => {
+            let ratio = image.width / image.height
+            return ratio > 1 ? {
+                height: resolution,
+                width: Math.floor(ratio*resolution)
+            } : {
+                width: resolution,
+                height: Math.floor(resolution/ratio)
+            }
+        }
 
-    }).data
+        const getImageData256 = (canvas) => {
 
-    var imageData = new ImageData(new Uint8ClampedArray(data), HEIGHT, WIDTH)
-    ctxSprite.putImageData(imageData, 0, 0)
-    window
-      .localStorage
-      .setItem('sprite', JSON.stringify(imageData))
+            let colorData = canvas.getContext('2d')
+                .getImageData(0, 0, canvas.width, canvas.height)
+                .data
 
-    // window.requestAnimationFrame = (function () {     return
-    // window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
-    // window.mozRequestAnimationFrame || function (callback) {
-    // window.setTimeout(callback, 1000 / 60);     }; })();
+            // TODO: remove temp and improve algorithm
+            let data = colorData.reduce((acc, curr, index, arr) => {
+                // let key = Math.floor(index / 4)
+                acc.temp.push(curr)
+                if (acc.temp.length === 4) {
+                    const ansi = convert.rgb.ansi256(...acc.temp)
+                    const rounded = convert.ansi256.rgb(ansi).concat(255)
+                    acc.data = acc.data.concat(rounded)
+                    acc.temp = []
+                }
+                return acc
+            }, {
+                temp: [],
+                data: []
+            }).data
+            return new ImageData(new Uint8ClampedArray(data), canvas.width, canvas.height)
 
-    CanvasHelper.drawGrid()
-  }
+        }
 
-  eidtorMode() {}
-  // var uri = canvas.toDataURL('image/png');
-  // $('#draw-bg').css('background-image', 'url(' + uri + ')');
+        const drawPixels = (obj, scale) => {
+            let {width,height}=obj.dimension
+            let image =obj.image
+            this.sprite.width=width*scale
+            this.sprite.height=height*scale
+            ctxSprite.mozImageSmoothingEnabled = false;
+            ctxSprite.webkitImageSmoothingEnabled = false;
+            ctxSprite.imageSmoothingEnabled = false;
+            ctxSprite.drawImage(image, 0, 0, image.width, image.height, 0, 0, width,height)
+      
+            ctxSprite.drawImage(this.sprite, 0, 0, width, height, 0, 0, width * scale, height * scale)
+            // context.drawImage(image, 0, 0, preview.width, preview.height) round to 256
+            // ctxSprite.putImageData(imageData, 0, 0)
+            // window.localStorage.setItem('sprite', JSON.stringify(imageData))
 
-  componentWillUnmount() {}
-  render() {
-    return (
-      <div className="app">
-        {/* control the conversion scale */}
-        <label>Resolution
-          <input
-            type="range"
-            ref={el => this.resolution = el}
-            name="resolution"
-            min="1"
-            max="3"/>
-        </label>
-        <canvas id="sprite" ref= { el => this.sprite = el }></canvas >
-        <input type="file" ref= { el => this.file = el } id="file-upload"/>
-        <section id="editor">
-          <canvas ></canvas>
-        </section>
-      </div>
-    )
-  }
+            // window.requestAnimationFrame = (function () {     return
+            // window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
+            // window.mozRequestAnimationFrame || function (callback) {
+            // window.setTimeout(callback, 1000 / 60);     }; })();
+        }
+
+
+        fileUploaded.switchMap(file => {
+                if (file.type && file.type.indexOf("image") === 0)
+                    return readerFile(file)
+                return Observable.never()
+            })
+            .switchMap(imageData => imageLoaded(imageData))
+            .map(image =>({
+                dimension: getOptimisedDimension(image),
+                image
+            }))
+            .switchMap(value=>scaleChange.startWith(1),(inner,outer)=>[inner,outer]).map(([obj,scale])=>
+                drawPixels(obj, scale))
+            
+            .subscribe(console.log)
+
+        // CanvasHelper.drawGrid()
+    }
+schange(){}
+    eidtorMode() {}
+    // var uri = canvas.toDataURL('image/png');
+    // $('#draw-bg').css('background-image', 'url(' + uri + ')');
+
+    componentWillUnmount() {}
+    render() {
+        return ( <div className = "app"> { /* control the conversion scale */ } <label> Resolution 
+            <input type="range" ref={ el => this.scale = el} name="scale"
+            min = "1"
+            max = "3" />
+            </label> <canvas id="sprite" ref={el => this.sprite = el} > </canvas >
+             <input type = "file" ref = { el => this.fileUpload = el } id = "file-upload"/> 
+             <section id = "editor">
+             <canvas></canvas> 
+             </section> </div>
+        )
+    }
 }
 export default App;
